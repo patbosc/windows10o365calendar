b869462f1e42dd9f3f33f996a2a7896252032654
Patric Boscolo <patbosc@microsoft.com>
2015-12-19 12:50:30 +0000

    Implemented Authentication Helpers

---
+++ o365calendar/o365calendar/Helpers/ExctendedHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace o365calendar.Helpers
+{
+    public static class Extensions
+    {
+        public static void WriteStringWithLength(this DataWriter w, string s)
+        {
+            w.WriteUInt32((uint)s.Length);
+            w.WriteString(s);
+        }
+
+        public static string ReadString(this DataReader r)
+        {
+            return r.ReadString(r.ReadUInt32());
+        }
+    }
+}
